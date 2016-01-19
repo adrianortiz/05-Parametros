a7c34dc1bc8a6f0040a0b892b64f6ba7f6bf7a61
Codizer <adrianortizmartinez@gmail.com>
2016-01-19 00:57:02 -0600

    Create project 05-Parametros

---
+++ src/AppParametros.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author Adrian
+ *
+ */
+public class AppParametros {
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Parametros p = new Parametros();
+		
+		System.out.printf("El perimetro del cuadrado es %d", p.cuadrado(10));
+		
+	}
+}
---
+++ src/Parametros.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Adrian
+ *
+ */
+public class Parametros {
+	
+	/**
+	 * Método que retorna la suma de su parametro cuatro veces
+	 * 
+	 * @param lado Lado de tipo Int
+	 * @return La suma de la variable lado cuatro veces
+	 */
+	public int cuadrado(int lado) {
+		return lado + lado + lado + lado;
+	}
+}
